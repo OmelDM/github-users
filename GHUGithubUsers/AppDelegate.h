e4f63290fd4f85bce38eafcb05f045bc6d309057
Dmytro Omelchuk <omeldm@gmail.com>
2015-04-06 12:57:13 +0300

    Initial commit

---
+++ GHUGithubUsers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GHUGithubUsers
+//
+//  Created by Dmytro Omelchuk on 4/6/15.
+//  Copyright (c) 2015 Dmytro Omelchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GHUGithubUsers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GHUGithubUsers
+//
+//  Created by Dmytro Omelchuk on 4/6/15.
+//  Copyright (c) 2015 Dmytro Omelchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
